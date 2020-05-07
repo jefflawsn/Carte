a575349a902e85910a007deca8a384ffbe443974
Jeff <jeffreyson18@gmail.com>
2020-05-07 10:02:31 +0200

    Ajout de Angular + Carte

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  map: Map;
+
+  ionViewDidEnter() {
+    this.leafletMap();
+  }
+
+  leafletMap() {
+    this.map = new Map('mapId').setView([48.8534, 2.3488], 13);
+
+    tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com'
+    }).addTo(this.map);
+
+    const markPoint = marker([12.972442, 77.594563]);
+    markPoint.bindPopup('<p>Tashi Delek - Bangalore.</p>');
+    this.map.addLayer(markPoint);
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
+  }
+
+}
